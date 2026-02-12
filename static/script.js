eba602beeb22e451e72837a6124bdbcd3ea80ed8
Vasu Gadde <vasu.gadde@JHS.LOCAL>
2026-02-12 21:22:18 +0530

    Updated script.js

--- static/script.js
+++ static/script.js
@@ -4267,8 +4267,4 @@ function updateApproveAllButtons() {
         rejectedRows > 0 ? "block" : "none";
 }
 
-// Call this after every load of the tables
-// (add it at the end of loadPendingList() and loadRejectedList())
-
-
 /* End of file */
